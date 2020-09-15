d4ce9550d31db3491c7ba17f437f863ca7ce7e34
BryanFM <bryan.floresmagallanes@mail.com>
2020-09-14 23:10:42 -0500

    master-commit

--- interfaz.py
+++ interfaz.py
@@ -1,41 +1,59 @@
 from connection.conn import Connection
+from time import sleep
 from pprint import pprint
 
-def menu():
-    print('Bienvenido al gestor de datos del colegio Perez de Cuellar')
-    print('Que seleccione la acción que desea realizar: ')
-    print('\t1 - Registrar Docentes')
-    print('\t2 - Registrar Alumnos')
-    print('\t3 - Gestionar catedras de Docentes')
-    print('\t4 - Gestionar notas de Alumnos')
-    print('\t5 - Ver Docentes')
-    print('\t6 - Ver Alumnos')
-    print('\t7 - Ver notas')
-    print('\t8 - Salir')
-
-def continuar(self):
-    pass
-
-def insert_usuario(self):
-    pass
-
-def insert_catedras(self):
-    pass
-
-def insert_notas(self):
-    pass
-
-def mostrar_docentes(self):
-    pass
-
-def mostrar_alumnos(self):
-    pass
-
-def mostrar_notas(self):
-    pass
-
-
 c = Connection()
-usuarios = c.execute_query('Select * from usuario')
-list_usuarios = list(usuarios)
-pprint(list_usuarios)
+def menu(c):
+    while True:
+        print('Bienvenido al gestor de datos del colegio Perez de Cuellar')
+        #docentes
+        #alumnos
+        #salones
+        #cursos
+        #notas
+        #periodo_escolar
+        print('Seleccione la acción que desea realizar: ')
+        print('\t1 - Registrar Docente')
+        print('\t2 - Registrar Alumno')
+        print('\t3 - Gestionar catedras de Docentes')
+        print('\t4 - Gestionar notas de Alumnos')
+        print('\t5 - Ver Docentes')
+        print('\t6 - Ver Alumnos')
+        print('\t7 - Ver notas')
+        print('\t8 - Salir')
+        opcion = input("> ")
+        if opcion == "1":
+            mostrar_alumnos(c)
+        elif opcion == "6":
+            mostrar_alumnos(c)
+        elif opcion == "8":
+            print("\nGracias por usar esta aplicación")
+            sleep(2)
+            quit()
+        else:
+            print("\nHas introducido una opción errónea")
+
+
+def continuar():
+    pass
+
+def insert_usuario():
+    pass
+
+def insert_catedras():
+    pass
+
+def insert_notas():
+    pass
+
+def mostrar_docentes():
+    pass
+
+def mostrar_alumnos(c):
+    usuarios = c.execute_query('Select * from usuario')
+    list_usuarios = list(usuarios)
+    pprint(list_usuarios)
+    pass
+
+def mostrar_notas():
+    pass
